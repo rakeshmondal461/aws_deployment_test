bd6481edf16977f12a848d7f06cdf5de33021176
rakeshmondal461 <mondalrakesh461@gmail.com>
2024-03-03 18:00:58 +0530

    log ip

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
+  const clientIP =
+    (req.headers["x-forwarded-for"] || "").split(",").pop().trim() ||
+    req.socket.remoteAddress;
+  console.log("IP>>>", req.ip);
+  console.log("clientIP>>>", clientIP, "<<<clientIP");
   return res.status(200).json({ message: "REQUEST SERVED" });
 });
 
